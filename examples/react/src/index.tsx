fb61d0f2fb6d68bca294c371de701850b86b2a37
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-18 14:49:43 +0800

    refactor(examples): move code to src, rename

---
+++ examples/react/src/index.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+
+// Mount the React component to a DOM element with id="app"
+createRoot(document.getElementById('app')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
